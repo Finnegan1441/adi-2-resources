13cd5a4913763de7abe83e5cf01d5c9ebd0ae446
Patrick Cummins <patrickcummins2009@gmail.com>
2016-07-06 11:41:25 -0700

    Dynamic Fragments

--- week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/DynamicFragment/app/src/main/java/ly/generalassemb/dynamicfragment/MainActivity.java
@@ -1,6 +1,8 @@
 package ly.generalassemb.dynamicfragment;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +18,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setupButtons();
 
         // TODO: setup fragment manager and transaction
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         // TODO: create dailyForecast fragment
+        FragmentDaily fragmentDaily = new FragmentDaily();
 
         // TODO: add the daily forecast fragment to the container and commit the change
-
+        fragmentTransaction.add(R.id.myContainer, fragmentDaily);
+        fragmentTransaction.commit();
     }
 
     private void setupButtons(){
@@ -33,13 +39,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         switch (v.getId()){
             case R.id.daily_button:
                 // todo replace with dailyWeather fragment
+
+
+
+                FragmentDaily fragmentDaily = new FragmentDaily();
+
+
+                fragmentTransaction.add(R.id.myContainer, fragmentDaily);
+                fragmentTransaction.commit();
                 break;
             case R.id.weekly_button:
                 // todo replace with weeklyWeather fragment
+
+
+               FragmentWeek fragmentWeek = new FragmentWeek();
+
+
+                fragmentTransaction.add(R.id.myContainer, fragmentWeek);
+                fragmentTransaction.commit();
                 break;
             default:
                 break;
