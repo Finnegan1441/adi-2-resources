6b77d16fc95d535bb497c44d95e00cb23ba5e433
Patrick Cummins <patrickcummins2009@gmail.com>
2016-07-06 10:51:49 -0700

    Static Fragments

--- week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/StaticFragment/app/src/main/java/ly/generalassemb/staticfragment/MainActivity.java
+++ week-5-fragments/23-wednesday-fragments/lessons/fragments-1-lesson/starter-code/StaticFragment/app/src/main/java/ly/generalassemb/staticfragment/MainActivity.java
@@ -1,7 +1,9 @@
 package ly.generalassemb.staticfragment;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Fragment myFrag = (Fragment) getSupportFragmentManager().findFragmentById(R.id.yee2);
+
+        TextView fragText = (TextView) myFrag.getView().findViewById(R.id.mTextView);
+        fragText.setText("Anotha One");
     }
 }
