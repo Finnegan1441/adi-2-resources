ea768d21fe4aec6992d220340a65967a331b1f4d
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-20 12:28:38 -0700

    Finished

--- week-7-networking/33-wednesday-threading/labs/thread-safe-app-lab/starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
+++ week-7-networking/33-wednesday-threading/labs/thread-safe-app-lab/starter-code/app/src/main/java/generalassembly/yuliyakaleda/solution_code_thread_safe/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity{
     if (requestCode == PICK_IMAGE_REQUEST && resultCode == MainActivity.RESULT_OK && null != data) {
       Uri selectedImage = data.getData();
       //TODO: Create the async task and execute it
+      new ImageProcessingAsyncTask().execute(selectedImage);
     }
   }
 
@@ -62,11 +63,11 @@ public class MainActivity extends AppCompatActivity{
   }
 
   //TODO: Fill in the parameter types
-  private class ImageProcessingAsyncTask extends AsyncTask<> {
+  private class ImageProcessingAsyncTask extends AsyncTask<Uri, Void, Bitmap> {
 
     //TODO: Fill in the parameter type
     @Override
-    protected Bitmap doInBackground() {
+    protected Bitmap doInBackground(Uri... params) {
       try {
         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(params[0]));
         return invertImageColors(bitmap);
@@ -78,20 +79,25 @@ public class MainActivity extends AppCompatActivity{
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onProgressUpdate() {
+    protected void onProgressUpdate(Void... values) {
       super.onProgressUpdate(values);
       //TODO: Update the progress bar
     }
 
     //TODO: Fill in the parameter type
     @Override
-    protected void onPostExecute() {
+    protected void onPostExecute(Bitmap bitmap) {
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mImageView.setImageBitmap(bitmap);
       //TODO: Complete this method
     }
 
     @Override
     protected void onPreExecute() {
       super.onPreExecute();
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBar.setProgress(0);
       //TODO: Complete this method
     }
 
@@ -113,6 +119,7 @@ public class MainActivity extends AppCompatActivity{
         }
         int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
         //TODO: Update the progress bar. progressVal is the current progress value out of 100
+          mProgressBar.setProgress(progressVal);
       }
       return mutableBitmap;
     }
