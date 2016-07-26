f63717c79f18138498eb20f32ac53c04231e1fff
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-26 09:35:26 -0700

    RecyclerView Independent Practice

--- week-8-services/37-tuesday-recyclerview/lessons/recycler-view-part-1/starter-code/app/src/main/java/ly/generalassemb/recyclerview/MainActivity.java
+++ week-8-services/37-tuesday-recyclerview/lessons/recycler-view-part-1/starter-code/app/src/main/java/ly/generalassemb/recyclerview/MainActivity.java
@@ -2,9 +2,19 @@ package ly.generalassemb.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     // TODO Step 3, declare variable here
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter rvAdapter;
+    private RecyclerView.LayoutManager rvLayoutManager;
+
+    private LinkedList<String> dataList = new LinkedList<String>();
 
 
 
@@ -18,17 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Step 4a) find recycler view from xml
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
 
 
         // TODO: Step 4b) add data to our list
-
+        dataList.add("Arizona");
+        dataList.add("California");
+        dataList.add("New Mexico");
+        dataList.add("New York");
 
 
         // TODO: Step 4c) create and set linear layout manager on recycler view
+        //rvLayoutManager = new LinearLayoutManager(this);
+        rvLayoutManager = new GridLayoutManager(this, 2);
+        //rvLayoutManager = new StaggeredGridLayoutManager(this, null, 2, 2);
+        recyclerView.setLayoutManager(rvLayoutManager);
 
 
 
         // TODO: Step 4d) create the custom adapter with our data and set it on the recyclerView
+        rvAdapter = new CustomRecyclerViewAdapter(dataList);
+        recyclerView.setAdapter(rvAdapter);
 
 
 
