02002a05566403efda61c79449dc19cd3f255c59
Finnegan1441 <patrickcummins2009@gmail.com>
2016-07-27 11:29:22 -0700

    Content Provider Independent Practice

--- week-8-services/38-wednesday-content-provider/lessons/content-providers/starter-code/ContentProvider/app/src/main/java/generalassembly/yuliyakaleda/contentprovider/MainActivity.java
+++ week-8-services/38-wednesday-content-provider/lessons/content-providers/starter-code/ContentProvider/app/src/main/java/generalassembly/yuliyakaleda/contentprovider/MainActivity.java
@@ -13,67 +13,90 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
-  public static final Uri CONTENT_URI = Uri.parse("content://generalassembly.yuliyakaleda.contentprovider.MyContentProvider/products");
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final Uri CONTENT_URI = Uri.parse("content://generalassembly.yuliyakaleda.contentprovider.MyContentProvider/products");
 
-  private EditText mInputName;
-  private EditText mInputQuantity;
-  private TextView mResultTextView;
+    private EditText mInputName;
+    private EditText mInputQuantity;
+    private TextView mResultTextView;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    Button add = (Button) findViewById(R.id.add_button);
-    Button find = (Button) findViewById(R.id.find_button);
-    Button delete = (Button) findViewById(R.id.delete_button);
-    Button update = (Button) findViewById(R.id.update_button);
+        Button add = (Button) findViewById(R.id.add_button);
+        Button find = (Button) findViewById(R.id.find_button);
+        Button delete = (Button) findViewById(R.id.delete_button);
+        Button update = (Button) findViewById(R.id.update_button);
 
-    add.setOnClickListener(this);
-    find.setOnClickListener(this);
-    delete.setOnClickListener(this);
-    update.setOnClickListener(this);
+        add.setOnClickListener(this);
+        find.setOnClickListener(this);
+        delete.setOnClickListener(this);
+        update.setOnClickListener(this);
 
-    mInputName = (EditText) findViewById(R.id.input_name);
-    mInputQuantity = (EditText) findViewById(R.id.input_quantity);
-    mResultTextView = (TextView) findViewById(R.id.result);
-  }
-
-  //Use content provider's methods!
-  public void addProduct () {
-
-  }
-
-  public void lookupProduct () {
-
-  }
-
-  public void removeProduct () {
-
-  }
-
-  public void updateProduct(){
-
-  }
-
-  @Override
-  public void onClick(View v) {
-    switch (v.getId()) {
-      case R.id.add_button:
-        addProduct();
-        break;
-      case R.id.find_button:
-        lookupProduct();
-        break;
-      case R.id.delete_button:
-        removeProduct();
-        break;
-      case R.id.update_button:
-        updateProduct();
-        break;
-      default:
-        break;
+        mInputName = (EditText) findViewById(R.id.input_name);
+        mInputQuantity = (EditText) findViewById(R.id.input_quantity);
+        mResultTextView = (TextView) findViewById(R.id.result);
     }
-  }
+
+    //Use content provider's methods!
+    public void addProduct() {
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put("productname", mInputName.getText().toString());
+        values.put("quantity", mInputQuantity.getText().toString());
+        contentResolver.insert(CONTENT_URI, values);
+        mResultTextView.setText("Added " + mInputName.getText().toString());
+
+    }
+
+    public void lookupProduct() {
+        ContentResolver contentResolver = getContentResolver();
+//        ContentValues values = new ContentValues();
+//        values.put("productname", mInputName.getText().toString());
+        Cursor cursor = contentResolver.query(CONTENT_URI, null, mInputName.getText().toString(), null, null );
+
+
+    }
+
+    public void removeProduct() {
+        ContentResolver contentResolver = getContentResolver();
+//        ContentValues values = new ContentValues();
+//        values.put("productname", mInputName.getText().toString());
+        contentResolver.delete(CONTENT_URI, mInputName.getText().toString(), null);
+        mResultTextView.setText("Removed " + mInputName.getText().toString());
+
+
+    }
+
+    public void updateProduct() {
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put("productname", mInputName.getText().toString());
+        values.put("quantity", mInputQuantity.getText().toString());
+        contentResolver.update(CONTENT_URI, values, mInputName.getText().toString(), null);
+        mResultTextView.setText("Updated " + mInputName.getText().toString());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_button:
+                addProduct();
+                break;
+            case R.id.find_button:
+                lookupProduct();
+                break;
+            case R.id.delete_button:
+                removeProduct();
+                break;
+            case R.id.update_button:
+                updateProduct();
+                break;
+            default:
+                break;
+        }
+    }
 }
